48d066c9586a7bbcffb97a77a3a47625c347df3b
Ljxtt <1058648620@qq.com>
2020-09-13 18:17:23 +0800

    update

--- 图论/网络流/Dinic.cpp
+++ 图论/网络流/Dinic.cpp
@@ -1,3 +1,15 @@
+#include <bits/stdc++.h>
+#define lowbit(x) ((x)&(-x))
+#define mem(i, a) memset(i, a, sizeof(i))
+#define sqr(x) ((x)*(x))
+#define ls(x) (x << 1)
+#define rs(x) (x << 1 | 1)
+typedef long long ll;
+const double eps = 1e-8;
+const double pi = acos(-1.0);
+const int inf = 0x3f3f3f3f;
+const int maxn = 1e5 + 7;
+using namespace std;
 int n, m, s, t, tot;
 int head[maxn], nex[maxn], edge[maxn], ver[maxn], d[maxn];
 void add(int x, int y, int z){
@@ -37,4 +49,28 @@ int dinic(int s, int flow){
         }
     }
     return flow - rest;
+}
+void show(void){
+    for(int i = 1; i <= n; i++){
+        for(int j = head[i]; j; j = nex[j]){
+            printf("%d %d %d\n", i, ver[j], edge[j]);
+        }
+    }
+}
+int main(void){
+#ifdef ljxtt
+freopen("data.in", "r", stdin);
+#endif
+    scanf("%d%d%d%d", &n, &m, &s, &t);
+    tot = 1;
+    for(int i = 0, x, y, z; i < m; i++){
+        scanf("%d%d%d", &x, &y, &z);
+        add(x, y, z);
+    }
+    ll maxflow = 0;
+    while(bfs()){
+        maxflow += dinic(s, inf);
+    }
+    printf("%lld\n", maxflow);   
+    return 0;
 }
