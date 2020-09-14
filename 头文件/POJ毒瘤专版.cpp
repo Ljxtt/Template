3788eeb415f397016092b986aa8cf7b2b662d5bf
Ljxtt <1058648620@qq.com>
2020-09-14 14:24:10 +0800

    update

---
+++ 头文件/POJ毒瘤专版.cpp
@@ -0,0 +1,28 @@
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#define lowbit(x) ((x)&(-x))
+#define mem(i, a) memset(i, a, sizeof(i))
+#define sqr(x) ((x)*(x))
+#define ls(x) (x << 1)
+#define rs(x) (x << 1 | 1)
+typedef long long ll;
+const double eps = 1e-8;
+const double pi = acos(-1.0);
+const int inf = 0x3f3f3f3f;
+const int maxn = e + 7;
+using namespace std;
+int main(void){
+#ifdef ljxtt
+freopen("data.in", "r", stdin);
+#endif
+    
+    return 0;
+}
