f3298190b8260a99e0bac545b31ae78854807b6b
Ljxtt <42873162+Ljxtt@users.noreply.github.com>
2020-09-28 23:45:12 +0800

    Update Dinic.cpp
    代码风格同一

--- 图论/网络流/Dinic.cpp
+++ 图论/网络流/Dinic.cpp
@@ -15,17 +15,18 @@ const double pi = acos(-1.0);
 const int inf = 0x3f3f3f3f;
 const int maxn = 1e5 + 7;
 using namespace std;
-int n, m, s, t, tot;
-int head[maxn], nex[maxn], edge[maxn], ver[maxn], d[maxn];
-void add(int x, int y, int z){
+int head[maxn], nex[maxn], edge[maxn], ver[maxn], d[maxn], tot = 0;
+void add_edge(int x, int y, int z){
     ver[++tot] = y; edge[tot] = z; nex[tot] = head[x]; head[x] = tot;
-    ver[++tot] = x; edge[tot] = 0; nex[tot] = head[y]; head[y] = tot;
+}
+void add(int x, int y, int z){
+    add_edge(x, y, z);
+    add_edge(y, x, 0);
 }
 queue<int> que;
-bool bfs(void){
+bool bfs(int s, int t){
     while(que.size()) que.pop();
-    mem(d, 0);
-    d[s] = 1;
+    mem(d, 0); d[s] = 1;
     que.push(s); 
     while(que.size()){
         int now = que.front(); que.pop();
@@ -40,13 +41,13 @@ bool bfs(void){
     }
     return 0;
 }
-int dinic(int s, int flow){
+int dfs(int s, int t, int flow){
     if(s == t) return flow;
     int rest = flow, k;
     for(int i = head[s]; i && rest; i = nex[i]){
         int ne = ver[i];
         if(edge[i] && d[ne] == d[s] + 1){
-            k = dinic(ne, min(rest, edge[i]));
+            k = dfs(ne, t, min(rest, edge[i]));
             if(!k) d[ne] = 0;
             edge[i] -= k;
             edge[i ^ 1] += k;
@@ -55,20 +56,22 @@ int dinic(int s, int flow){
     }
     return flow - rest;
 }
+ll dinic(int s, int t){
+    ll maxflow = 0, flow = 0;
+    while(bfs(s, t)) while(flow = dfs(s, t, inf)) maxflow += flow;
+    return maxflow;
+}
 int main(void){
 #ifdef ljxtt
 freopen("data.in", "r", stdin);
 #endif
+    int n, m, s, t;
     scanf("%d%d%d%d", &n, &m, &s, &t);
     tot = 1;
     for(int i = 0, x, y, z; i < m; i++){
         scanf("%d%d%d", &x, &y, &z);
         add(x, y, z);
     }
-    ll maxflow = 0;
-    while(bfs()){
-        maxflow += dinic(s, inf);
-    }
-    printf("%lld\n", maxflow);   
+    printf("%lld\n", dinic(s, t));   
     return 0;
 }
